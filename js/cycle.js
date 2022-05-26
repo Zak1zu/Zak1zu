92b873011c25868b3eed88c4056eed27b370c5a7
Zak1zu <bayer.erich7@gmail.com>
2022-05-26 09:46:01 +0300

    four commit

--- js/cycle.js
+++ js/cycle.js
@@ -20,3 +20,38 @@ for (let i = 1; i < 10; i++) {
     }
     console.log(i);
 }
+
+for (let i = 0; i < 3; i++) {
+    console.log(i);
+    for (let j = 0; j < 3; j++) {
+        console.log(j); 
+    }
+}
+
+let result = '';
+const length1 = 7;
+
+for (let i = 1; i < length1; i++) {
+
+    for (let j = 0; j < i; j++) {
+        result += '*';
+    }
+
+    result += '\n';
+}
+
+
+
+console.log(result);
+
+first: for (let i = 0; i < 3; i++) {
+    console.log(`First level: ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`);
+        for (let k = 0; k < 3; k++) {
+            if (k === 2) {
+                break first;}
+            console.log(`Third level: ${k}`);
+        }
+    }
+}
